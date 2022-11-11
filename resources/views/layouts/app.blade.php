1435f3fe919c33d5dd1301964790544856d14c2e
Leon McCarthy <mccarthymu@ueab.ac.ke>
2022-11-11 16:35:09 +0300

    returned app and guest layout to default state

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,10 +11,10 @@
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Scripts -->
-        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
-        <link href="/build/assets/app.6bd3cb76.css" rel="stylesheet">
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        {{-- <link href="/build/assets/app.6bd3cb76.css" rel="stylesheet">
 
-        <script src="{{ asset('/build/assets/app.f10bc5fc.js') }}" defer></script>
+        <script src="{{ asset('/build/assets/app.f10bc5fc.js') }}" defer></script> --}}
 
         <!-- Styles -->
         @livewireStyles
